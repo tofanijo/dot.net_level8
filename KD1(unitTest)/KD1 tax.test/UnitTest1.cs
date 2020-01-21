eb191c9abc0f625be2cc80ae0d0c995fbd352f8e
tofanijo <tofanijo@gmail.com>
2020-01-21 19:58:43 +0200

    KD1(unitTest)

---
+++ KD1(unitTest)/KD1 tax.test/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KD1_unitTest_;
+
+namespace KD1_tax.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FunkcijaApverstiSakyni_TuriApverstiSakyni()
+        {
+            //Arange
+            string sakinysnr1 = "abc";
+            //Act
+            string rezultatas = Program.ApverstiSakini(sakinysnr1);
+            //Asert
+            Assert.AreEqual(rezultatas, "cba");
+        }
+        [TestMethod]
+        public void FunkcijaApverstiSakyniSuTusciaReiksme_TuriGRazintiTusciaReiksme()
+        {
+            //Arange
+            string sakinysnr1 = "";
+            //Act
+            string rezultatas = Program.ApverstiSakini(sakinysnr1);
+            //Asert
+            Assert.AreEqual(rezultatas, "");
+        }
+        [TestMethod]
+        public void FunkcijaApverstiSakyniSuTarpu_TuriGRazintiTarpa()
+        {
+            //Arange
+            string sakinysnr1 = " ";
+            //Act
+            string rezultatas = Program.ApverstiSakini(sakinysnr1);
+            //Asert
+            Assert.AreEqual(rezultatas, " ");
+        }
+    }
+}
---
+++ KD1(unitTest)/KD1(unitTest)/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KD1_unitTest_
+{
+    public class Program
+    {
+        public static string ApverstiSakini(string sakynis)
+        {
+            string rezultatas = "";
+
+            for (int i = sakynis.Length - 1; i >= 0; i--)
+            {
+                rezultatas = rezultatas + sakynis[i].ToString();
+            }
+            return rezultatas;
+        }
+
+        static void Main(string[] args)
+        {
+            //Sukurti funkciją kuri
+            //apverčia stringą ir parašyti
+            //jai testus su ilgu stringu, su
+            //stringu iš 1 simbolio, su
+            //tuščiu stringu
+
+            Console.WriteLine("Iveskite sakyni");
+            string sakynis = Console.ReadLine();
+            string rezultatas = ApverstiSakini(sakynis);
+            Console.Write(rezultatas);
+            Console.ReadLine();
+
+        }
+
+    }
+}
